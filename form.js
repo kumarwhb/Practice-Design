d52347b616cd156c7714988bf9d4f5a6b5c6dcd8
kumarwhb <kumarasamy.whb@gmail.com>
2022-10-13 09:43:28 +0530

    heroicon

---
+++ form.js
@@ -0,0 +1,74 @@
+function jump(){
+    alert("hello, my name is kumar")
+}
+
+jump()
+<div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon3.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
+          <div class="icon3">
+            <img src="/icon2.svg" alt="check" width="30px" /> <span>honey</span>
+          </div>
